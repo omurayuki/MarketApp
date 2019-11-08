2b05c2b8a8753258beb6c68219f6ed6a8b8d5589
omurayuki <g1221438qz12@gmail.com>
2019-11-09 02:11:48 +0900

    ログイン, 登録機能実装

--- Market/MainView/WelcomeViewController.swift
+++ Market/MainView/WelcomeViewController.swift
@@ -1,4 +1,6 @@
 import UIKit
+import JGProgressHUD
+import NVActivityIndicatorView
 
 class WelcomeViewController: UIViewController {
 
@@ -6,25 +8,133 @@ class WelcomeViewController: UIViewController {
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var resendButton: UIButton!
     
+    let hud = JGProgressHUD(style: .dark)
+    var activityIdicator: NVActivityIndicatorView?
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        activityIdicator = NVActivityIndicatorView(frame: CGRect(x: self.view.frame.width / 2 - 30, y: self.view.frame.height / 2 - 30, width: 60.0, height: 60.0), type: .ballPulse, color: #colorLiteral(red: 0.9998469949, green: 0.4941213727, blue: 0.4734867811, alpha: 1.0), padding: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     
     @IBAction func cancelButtonPressed(_ sender: Any) {
-        
+        dismissView()
     }
     
     @IBAction func loginButtonPressed(_ sender: Any) {
+        if textFieldsHaveText() {
+            
+            loginUser()
+        } else {
+            hud.textLabel.text = "All fields are required"
+            hud.indicatorView = JGProgressHUDErrorIndicatorView()
+            hud.show(in: self.view)
+            hud.dismiss(afterDelay: 2.0)
+        }
     }
     
     @IBAction func registerButtonPressed(_ sender: Any) {
-        
+         if textFieldsHaveText() {
+             registerUser()
+         } else {
+             hud.textLabel.text = "All fields are required"
+             hud.indicatorView = JGProgressHUDErrorIndicatorView()
+             hud.show(in: self.view)
+             hud.dismiss(afterDelay: 2.0)
+         }
     }
     
     @IBAction func forgotPasswordButtonPressed(_ sender: Any) {
     }
     
     @IBAction func resendEmailButtonPressed(_ sender: Any) {
+    }
+    
+    private func registerUser() {
+        
+        showLoadingIdicator()
+        
+        MUser.registerUserWith(email: emailTextField.text!, password: passwordTextField.text!) { (error) in
+            
+            if error == nil {
+                self.hud.textLabel.text = "Varification Email sent!"
+                self.hud.indicatorView = JGProgressHUDSuccessIndicatorView()
+                self.hud.show(in: self.view)
+                self.hud.dismiss(afterDelay: 2.0)
+            } else {
+                print("error registering", error!.localizedDescription)
+                self.hud.textLabel.text = error!.localizedDescription
+                self.hud.indicatorView = JGProgressHUDErrorIndicatorView()
+                self.hud.show(in: self.view)
+                self.hud.dismiss(afterDelay: 2.0)
+            }
+            
+            
+            self.hideLoadingIdicator()
+        }
+        
+    }
+    
+    //MARK: - Login User
+    
+    private func loginUser() {
+        
+        showLoadingIdicator()
+        
+        MUser.loginUserWith(email: emailTextField.text!, password: passwordTextField.text!) { (error, isEmailVerified) in
+            
+            if error == nil {
+                
+                if  isEmailVerified {
+                    self.dismissView()
+                    print("Email is verified")
+                } else {
+                    self.hud.textLabel.text = "Please Verify your email!"
+                    self.hud.indicatorView = JGProgressHUDErrorIndicatorView()
+                    self.hud.show(in: self.view)
+                    self.hud.dismiss(afterDelay: 2.0)
+                }
+                
+            } else {
+                print("error loging in the iser", error!.localizedDescription)
+                self.hud.textLabel.text = error!.localizedDescription
+                self.hud.indicatorView = JGProgressHUDErrorIndicatorView()
+                self.hud.show(in: self.view)
+                self.hud.dismiss(afterDelay: 2.0)
+            }
+            
+            
+            self.hideLoadingIdicator()
+        }
+        
+    }
+    
+    //MARK: - Helpers
+    
+    private func dismissView() {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    private func textFieldsHaveText() -> Bool {
+        return (emailTextField.text != "" && passwordTextField.text != "")
+    }
+    
+    private func showLoadingIdicator() {
+        if activityIdicator != nil {
+            self.view.addSubview(activityIdicator!)
+            activityIdicator!.startAnimating()
+        }
+    }
+    
+    private func hideLoadingIdicator() {
+        if activityIdicator != nil {
+            activityIdicator!.removeFromSuperview()
+            activityIdicator!.stopAnimating()
+        }
     }
 }
--- Market/Model/MUser.swift
+++ Market/Model/MUser.swift
@@ -63,4 +63,104 @@ class MUser {
             purchasedItemIds = []
         }
     }
+    
+    class func currentId() -> String {
+        return Auth.auth().currentUser!.uid
+    }
+    
+    class func currentUser() -> MUser? {
+        if Auth.auth().currentUser != nil {
+            if let dictionary = UserDefaults.standard.object(forKey: kCURRENTUSER) {
+                return MUser(_dictionary: dictionary as! NSDictionary)
+            }
+        }
+        return nil
+    }
+    
+    //MARK: - Login func
+    
+    class func loginUserWith(email: String, password: String, completion: @escaping (_ error: Error?, _ isEmailVerified: Bool) -> Void) {
+        
+        Auth.auth().signIn(withEmail: email, password: password) { (authDataResult, error) in
+            
+            if error == nil {
+                
+                if authDataResult!.user.isEmailVerified {
+                    
+                    downloadUserFromFirestore(userId: authDataResult!.user.uid, email: email)
+                    completion(error, true)
+                } else {
+                    
+                    print("email is not varified")
+                    completion(error, false)
+                }
+                
+            } else {
+                completion(error, false)
+            }
+        }
+    }
+
+    
+    //MARK: - Register user
+    
+    class func registerUserWith(email: String, password: String, completion: @escaping (_ error: Error?) ->Void) {
+        
+        Auth.auth().createUser(withEmail: email, password: password) { (authDataResult, error) in
+            
+            completion(error)
+            
+            if error == nil {
+                
+                //send email verification
+                authDataResult!.user.sendEmailVerification { (error) in
+                    print("auth email verification error : ", error?.localizedDescription)
+                }
+            }
+        }
+    }
+}
+
+//MARK: - DownloadUser
+
+func downloadUserFromFirestore(userId: String, email: String) {
+    
+    FirebaseReference(.User).document(userId).getDocument { (snapshot, error) in
+        
+        guard let snapshot = snapshot else { return }
+        
+        if snapshot.exists {
+            print("download current user from firestore")
+            saveUserLocally(mUserDictionary: snapshot.data()! as NSDictionary)
+        } else {
+            //there is no user, save new in firestore
+            
+            let user = MUser(_objrctId: userId, _email: email, _firstName: "", _lastName: "")
+            saveUserLocally(mUserDictionary: userDictionaryFrom(user: user))
+            saveUserToFirestore(mUser: user)
+        }
+    }
+}
+
+//MARK: - Save user to firebase
+
+func saveUserToFirestore(mUser: MUser) {
+    
+    FirebaseReference(.User).document(mUser.objectId).setData(userDictionaryFrom(user: mUser) as! [String : Any]) { (error) in
+        
+        if error != nil {
+            print("error saving user \(error!.localizedDescription)")
+        }
+    }
+}
+
+
+func saveUserLocally(mUserDictionary: NSDictionary) {
+    
+    UserDefaults.standard.set(mUserDictionary, forKey: kCURRENTUSER)
+    UserDefaults.standard.synchronize()
+}
+
+func userDictionaryFrom(user: MUser) -> NSDictionary {
+    return NSDictionary(objects: [user.objectId, user.email, user.firstName, user.lastName, user.fullName, user.fullAddress ?? "", user.onBoard, user.purchasedItemIds], forKeys: [kOBJECTID as NSCopying, kEMAIL as NSCopying, kFIRSTNAME as NSCopying, kLASTNAME as NSCopying, kFULLNAME as NSCopying, kFULLADDRESS as NSCopying, kONBOARD as NSCopying, kPURCHASEDITEMIDS as NSCopying])
 }
