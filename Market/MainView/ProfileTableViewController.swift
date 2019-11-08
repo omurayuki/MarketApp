ad225888c5d222b8911e1ee1a30ef474f296f03c
omurayuki <g1221438qz12@gmail.com>
2019-11-09 03:28:21 +0900

    onBoard画面作成

---
+++ Market/MainView/ProfileTableViewController.swift
@@ -0,0 +1,94 @@
+import UIKit
+
+class ProfileTableViewController: UITableViewController {
+
+    @IBOutlet weak var finishRegistrationButtonOutlet: UIButton!
+    @IBOutlet weak var purchaseHistoryButtonOutlet: UIButton!
+    
+    var editBarButtonOutlet: UIBarButtonItem!
+    // MARK: - View LifeCycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.tableFooterView = UIView()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        //check logged in status
+        checkLoginStatus()
+        checkOnboardingStatus()
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    //MARK: - Helpers
+    
+    private func checkLoginStatus() {
+        
+        if MUser.currentUser() == nil {
+            createRightBarButton(title: "Login")
+        } else {
+            createRightBarButton(title: "Edit")
+        }
+    }
+
+    
+    private func createRightBarButton(title: String) {
+        
+        editBarButtonOutlet = UIBarButtonItem(title: title, style: .plain, target: self, action: #selector(rightBarButtonItemPressed))
+        
+        self.navigationItem.rightBarButtonItem = editBarButtonOutlet
+    }
+    
+    //MARK: - IBActions
+    
+    @objc func rightBarButtonItemPressed() {
+        
+        if editButtonItem.title == "Login" {
+            //show login vire
+            showLoginView()
+        } else {
+            //go to profile
+            goToEditProfile()
+        }
+    }
+    
+    private func checkOnboardingStatus() {
+        
+        if MUser.currentUser() != nil {
+            
+            if MUser.currentUser()!.onBoard {
+                finishRegistrationButtonOutlet.setTitle("Account is Active", for: .normal)
+                finishRegistrationButtonOutlet.isEnabled = false
+            } else {
+                
+                finishRegistrationButtonOutlet.setTitle("Finish registration", for: .normal)
+                finishRegistrationButtonOutlet.isEnabled = true
+                finishRegistrationButtonOutlet.tintColor = .red
+            }
+            
+        } else {
+            finishRegistrationButtonOutlet.setTitle("Logged out", for: .normal)
+            finishRegistrationButtonOutlet.isEnabled = false
+            purchaseHistoryButtonOutlet.isEnabled = false
+        }
+    }
+
+    private func showLoginView() {
+        
+        let loginView = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(identifier: "loginView")
+        
+        self.present(loginView, animated: true, completion: nil)
+    }
+    
+    private func goToEditProfile() {
+        print("edit profile")
+    }
+}
